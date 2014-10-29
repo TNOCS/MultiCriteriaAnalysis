791beb620602f64cba02801bfe3857673d801ef9
arnoud <damylen@gmail.com>
2014-10-29 23:09:53 +0100

    Added rating control

--- MultiCriteriaAnalysis/public/app/app.ts
+++ MultiCriteriaAnalysis/public/app/app.ts
@@ -101,6 +101,24 @@
         .controller('CriteriasCtrl', Criterias.CriteriasCtrl)
         .controller('SolutionsCtrl', Solutions.SolutionsCtrl)
         .controller('CreateSolutionDialogCtrl', Solutions.CreateSolutionDialogCtrl)
+        .controller('RatingDemoCtrl', function ($scope) {
+            $scope.rate = 7;
+            $scope.max = 10;
+            $scope.isReadonly = false;
+
+            $scope.hoveringOver = function (value) {
+                $scope.overStar = value;
+                $scope.percent = 100 * (value / $scope.max);
+            };
+
+            $scope.ratingStates = [
+                { stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle' },
+                { stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty' },
+                { stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle' },
+                { stateOn: 'glyphicon-heart' },
+                { stateOff: 'glyphicon-off' }
+            ];
+        })
         .filter('format', [
             '$filter', '$locale', function(filter, locale) {
                 return function(value, format) {
