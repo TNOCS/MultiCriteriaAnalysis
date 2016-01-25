211db85a0cdda6c3f19a12748ba3645015073e91
Erik Vullings <erik.vullings@gmail.com>
2016-01-25 17:47:05 +0100

    Removed unused tasks from gulp script.

--- MultiCriteriaAnalysis/gulpfile.js
+++ MultiCriteriaAnalysis/gulpfile.js
@@ -3,7 +3,7 @@
 // http://www.smashingmagazine.com/2014/06/11/building-with-gulp/
 
 // Configure gulp scripts
-var appName = 'csWebApp';
+var appName = 'mca';
 
 var gulp = require('gulp'),
     insert = require('gulp-insert'),
@@ -27,60 +27,20 @@ gulp.task('create_dist', function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('create_release_dist', function() {
-    var assets = useref.assets();
+// gulp.task('create_release_dist', function() {
+//     var assets = useref.assets();
 
-    return gulp.src('./public/*.html')
-        .pipe(assets)
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', minifyCss()))
-        .pipe(assets.restore())
-        .pipe(useref())
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('minify_csComp', function() {
-    // gulp.src('../../csWeb/csComp/dist/csComp.js')
-    //    .pipe(plumber())
-    //    .pipe(gulp.dest('public/js/cs'));
-    gulp.src('public/js/cs/csComp.js')
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('public/cs/js'));
-});
-
-gulp.task('include_js', function() {
-    gulp.src('../../csWeb/csComp/includes/js/**/*.*')
-        .pipe(debug({
-            title: 'include_js:'
-        }))
-        .pipe(plumber())
-        .pipe(gulp.dest('./public/cs/js'));
-});
-
-gulp.task('include_css', function() {
-    gulp.src('../../csWeb/csComp/includes/css/*.*')
-        .pipe(plumber())
-        .pipe(gulp.dest('./public/cs/css'));
-});
-
-gulp.task('include_images', function() {
-    gulp.src('../../csWeb/csComp/includes/images/*.*')
-        .pipe(plumber())
-        .pipe(gulp.dest('./public/cs/images/'));
-});
-
+//     return gulp.src('./public/*.html')
+//         .pipe(assets)
+//         .pipe(gulpif('*.js', uglify()))
+//         .pipe(gulpif('*.css', minifyCss()))
+//         .pipe(assets.restore())
+//         .pipe(useref())
+//         .pipe(gulp.dest('dist'));
+// });
 
 gulp.task('watch', function() {
-    gulp.watch('../../csWeb/csComp/js/**/*.js', ['built_csComp']);
-    gulp.watch('../../csWeb/csComp/js/**/*.d.ts', ['built_csComp.d.ts']);
-    gulp.watch('../../csWeb/csComp/**/*.tpl.html', ['create_templateCache']);
-    gulp.watch('../../csWeb/csComp/includes/**/*.css', ['include_css']);
-    gulp.watch('../../csWeb/csComp/includes/**/*.js', ['include_js']);
-    gulp.watch('../../csWeb/csComp/includes/images/*.*', ['include_images']);
+    // gulp.watch('../../csWeb/csComp/js/**/*.js', ['built_csComp']);
 });
 
-gulp.task('default', ['create_templateCache', 'built_csComp', 'built_csComp.d.ts', 'include_css', 'include_js', 'include_images', 'watch']);
+gulp.task('default', ['watch']);
